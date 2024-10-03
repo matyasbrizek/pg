5497893c6cd2375fbd6b23f23e432bb6b2964741
matyasbrizek <matyas.brizek@gmail.com>
2024-10-03 09:07:36 +0000

    přidání sude/liche

--- first.py
+++ first.py
@@ -1,4 +1,18 @@
+#prvni funkce
 def hello():
     print("Hello World!")
 
-hello()
+#hello()
+
+
+#funkce, která určí, jestli číslo je sudé nebo liché
+def sudy_nebo_lichy(cislo):
+    vysledek = cislo % 2
+
+    if vysledek == 0:
+        print("Číslo ", cislo, " je sudé")
+    else:
+        print("Číslo ", cislo, " je liché")
+
+sudy_nebo_lichy(5)
+sudy_nebo_lichy(1000000)
