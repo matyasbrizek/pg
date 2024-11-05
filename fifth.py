ce815835ec796d8d2f10fe9da14bf16347106498
matyasbrizek <matyas.brizek@gmail.com>
2024-11-05 16:12:10 +0000

    zmena

--- fifth.py
+++ fifth.py
@@ -1,20 +1,19 @@
-
 import sys
 
-# definice úvodních binárních sekvencí obrázkových souborů
+# Definice úvodních binárních sekvencí obrázkových souborů
 jpeg_header = b'\xff\xd8'
 gif_header1 = b'GIF87a'
 gif_header2 = b'GIF89a'
 png_header = b'\x89PNG\r\n\x1a\n'
-
 
 def read_header(file_name, header_length):
     """
     Tato funkce načte binární soubor z cesty file_name,
     z něj přečte prvních header_length bytů a ty vrátí pomocí return
     """
-    return b'xxx'
-
+    with open(file_name, "rb") as img_file:
+        header = img_file.read(header_length)
+    return header
 
 def is_jpeg(file_name):
     """
@@ -25,27 +24,29 @@ def is_jpeg(file_name):
     header = read_header(file_name, len(jpeg_header))
 
     # vyhodnoť zda je soubor jpeg
-
-    return False
-
+    return header.startswith(jpeg_header)
 
 def is_gif(file_name):
     """
-    Funkce zkusí přečíst ze souboru hlavičku obrázku jpeg,
+    Funkce zkusí přečíst ze souboru hlavičku obrázku gif,
     tu srovná s definovanými hlavičkami v proměnných gif_header1 a gif_header2
     """
-    # vyhodnoť zda je soubor gif
-    return False
+    # načti hlavičku souboru
+    header = read_header(file_name, len(gif_header1))
 
+    # vyhodnoť zda je soubor gif
+    return header == gif_header1 or header == gif_header2
 
 def is_png(file_name):
     """
-    Funkce zkusí přečíst ze souboru hlavičku obrázku jpeg,
+    Funkce zkusí přečíst ze souboru hlavičku obrázku png,
     tu srovná s definovanou hlavičkou v proměnné png_header
     """
-    # vyhodnoť zda je soubor png
-    return False
+    # načti hlavičku souboru
+    header = read_header(file_name, len(png_header))
 
+    # vyhodnoť zda je soubor png
+    return header.startswith(png_header)
 
 def print_file_type(file_name):
     """
@@ -60,8 +61,14 @@ def print_file_type(file_name):
     else:
         print(f'Soubor {file_name} je neznámého typu')
 
-
 if __name__ == '__main__':
-    # přidej try-catch blok, odchyť obecnou vyjímku Exception a vypiš ji
-    file_name = sys.argv[1]
-    print_file_type(file_name)
+    # Přidej try-catch blok, odchyť obecnou výjimku Exception a vypiš ji
+    try:
+        file_name = sys.argv[1]
+        print_file_type(file_name)
+    except IndexError:
+        print("Nebyly zadany soubory")
+    except FileNotFoundError:
+        print("Soubor neexistuje")
+    except Exception as e:
+        print(f"Nastala neočekávaná chyba: {e}")
