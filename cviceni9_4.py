3438870a59cd349ed1d82b48414fb48640c201e4
matyasbrizek <matyas.brizek@gmail.com>
2024-12-05 10:24:19 +0000

    zemnba

--- cviceni9_4.py
+++ cviceni9_4.py
@@ -17,7 +17,7 @@ def stahuj(url):
     
 def test_stahuj():
     requests.get = MagicMock(return_value = Result(200, "abc")) #Magic Mock vrací hodnotu, kterou mu nastavíme, díky tomu se nemusíme připojovat na internet, ale stejně se to otestuje
-    assert stahuj("https://python.cz/") == "abc"
+    assert stahuj("https://python.cz/") == "abc"                #Magic Mok jí ndokáže "nahradit" v té tříde, to co tam napíšeme bude výstupem 
     requests.get = MagicMock(return_value = Result(500, "abc"))
     assert stahuj("https://python.cz/") == ""
 
